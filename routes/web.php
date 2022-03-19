4d8e0ffe7a6d4a0b9c5b1df8a5198f5eb602c7c9
almiqdad abuetailah <al-miqdad@sajil.org>
2022-03-20 00:01:14 +0300

    register and login for user

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|--------------------------------------------   ------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -17,6 +17,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('user')->name('user.')->group(function(){
+    Route::middleware(['guest:web','PreventBackHistory'])->group(function(){
+        Route::view('/login','dashboard.user.login')->name('login');
+        Route::view('/register','dashboard.user.register')->name('register');
+        Route::post('/create',[UserController::class,'create'])->name('create');
+        Route::post('/check',[UserController::class,'check'])->name('check');
+    });
+    Route::middleware(['auth:web','PreventBackHistory'])->group(function(){
+        Route::view('/home','dashboard.user.home')->name('home');
+        Route::post('/logout',[UserController::class,'logout'])->name('logout');
+    });
+});
+
+
+
+
+
+
+
+
+
