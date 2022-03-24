a7c05cbdd076a1307f98a9633afce2fc7b8a0e8a
almiqdad abuetailah <al-miqdad@sajil.org>
2022-03-24 13:51:03 +0300

    fasdf

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -17,7 +17,7 @@ class AdminController extends Controller
             'email'=>'required|email|exists:admins,email',
             'password'=>'required|min:5|max:30'
         ],[
-            'email.exists'=>'This email is not exists on admin table '
+            'email.exists'=>'This email is not exists on admin table  '
         ]);
 
         $creds = $request->only('email','password');
